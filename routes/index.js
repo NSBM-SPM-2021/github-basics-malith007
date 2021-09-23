df147b5d865cd85b446e48f1a6666cd95c86d089
malith007 <68961838+malith007@users.noreply.github.com>
2021-09-23 23:53:27 +0530

    created all user view part

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var connection = require('../config/connection');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  connection.query('SELECT * FROM users', function (error, users) {
+    if (error) {
+      throw error;
+
+    } else {
+      res.render('index', { all_users: users });
+    }
+  })
+ }); 
+
+ 
 
 module.exports = router;
