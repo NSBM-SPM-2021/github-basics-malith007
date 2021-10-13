98e19e5917f6626d0ff2d602cdac8c5af19d77ee
malith007 <68961838+malith007@users.noreply.github.com>
2021-10-13 23:34:03 +0530

    Bug Fixed

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,19 @@ var router = express.Router();
 
 var connection = require('../config/connection');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   connection.query('SELECT * FROM users', function (error, users) {
     if (error) {
       throw error;
-
     } else {
       res.render('index', { all_users: users });
     }
   })
  }); 
-
  
  router.post('/adduser', function (req, res) {
-  const bookdata = {
-    useranem: req.body.username,
+  const user = {
+    username: req.body.username,
     email: req.body.email,
     phone: req.body.phone,
   }
@@ -33,15 +30,13 @@ router.get('/', function(req, res, next) {
 
 });
 
-module.exports = router;
+router.get('/deleteuser/:id', function (req, res) {
 
-router.post('/deleteuser', function (req, res) {
-
-  var idBooks = req.params.id;
-  connection2.query("delete from users where userid = ?", [userid], function (err, result) {
-    if (err) {
-      console.error("Error");
-    } else {
+  var userid = req.params.id;
+  connection.query("delete from users where id = ?", [userid], function (err, result) {
       res.redirect('/');
-    }
   });
+
+});
+
+module.exports = router;
